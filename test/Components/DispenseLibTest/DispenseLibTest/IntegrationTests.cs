9032c1d4cf9bfc6f488b9044aefacfa35bccdac8
mcounsell <mcounsell@ukrobotics.net>
2022-04-20 12:27:23 -0700

    noop

--- test/Components/DispenseLibTest/DispenseLibTest/IntegrationTests.cs
+++ test/Components/DispenseLibTest/DispenseLibTest/IntegrationTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
 using UKRobotics.D2.DispenseLib;
+using UKRobotics.MotorControllerLib;
 
 namespace UKRobotics.D2.DispenseLibTest
 {
@@ -21,6 +22,26 @@ namespace UKRobotics.D2.DispenseLibTest
                 Console.WriteLine(controller.ReadSerialIDFromDevice());
 
                 controller.RunDispense("14f7fcb6f7be8f4676bcbd4b7c262c56", "5c527097-5a5c-4a97-9263-02b05f9a95cd");
+
+            }
+
+
+        }
+
+        [Test]
+        [Explicit]
+        public void TestTemp()
+        {
+
+            using (D2Controller controller = new D2Controller())
+            {
+                controller.OpenComms("COM18", 115200);
+
+                
+                Console.WriteLine(controller.ControllerArms.ReadString(ControllerParam.UserData1));
+                Console.WriteLine(controller.ControllerArms.ReadString(ControllerParam.UserData1+1));
+                Console.WriteLine(controller.ControllerArms.ReadString(ControllerParam.UserData1+2));
+                Console.WriteLine(controller.ControllerArms.ReadString(ControllerParam.UserData1+3));
 
             }
 
